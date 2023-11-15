b1a1bcc3f7fc8aa67be9369f88b3be8dce9bf7c7
Bond Tran <bond@sbidm.com>
2023-11-15 22:43:24 +0700

    feat: first commit

---
+++ app/(root)/(home)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return <div>
+
+    <h1 className="text-[50px] font-bold">Next.js 13 With Javascript Mastery</h1>
+    <h3 className="font-spaceGrotesk text-[30px] font-semibold text-primary">Font Space Grotesk</h3>
+  </div>;
+};
+
+export default Home;
---
+++ types/global.ts
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export type RootLayoutProps = {
+  children: React.ReactNode;
+};
