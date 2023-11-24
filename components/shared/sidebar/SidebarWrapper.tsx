0bfe64d66967f6759b74351909fada9bdfa1ab1e
Bond Tran <bond@sbidm.com>
2023-11-24 17:20:02 +0700

    feat: implement animation framer motion lesson 1

--- components/shared/sidebar/SidebarWrapper.tsx
+++ components/shared/sidebar/SidebarWrapper.tsx
@@ -1,14 +1,16 @@
 import { ScrollArea } from '@/components/ui/scroll-area';
 import { cn } from '@/lib/utils';
 
-interface SidebarWrapperProps extends React.HTMLAttributes<HTMLDivElement> {}
+interface SidebarWrapperProps {
+  children: React.ReactNode;
+  className?: string;
+}
 
-const SidebarWrapper = ({ children, className, ...props }: SidebarWrapperProps) => {
+const SidebarWrapper = ({ children, className }: SidebarWrapperProps) => {
   return (
     <aside
-      {...props}
       className={cn(
-        'sm: sticky top-[80px] flex h-[calc(100vh-80px)] flex-col overflow-y-auto p-6 pt-12 dark:shadow-none lg:w-[240px]',
+        'sticky top-[80px] flex h-[calc(100vh-80px)] flex-col overflow-y-auto p-6 pt-12 dark:shadow-none lg:w-[240px]',
         className,
       )}
     >
